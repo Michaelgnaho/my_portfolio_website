82c2f806deebc4d370b32b54faa22ab152166112
Michael Gnaho <gnahomichael@gmail.com>
2024-07-28 20:33:37 +0100

    Deploy to Render

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,19 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const cors = require('cors');
-const nodemailer = require('nodemailer');
-const path = require('path');
-require('dotenv').config();
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
 const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -24,11 +24,11 @@ contactEmail.verify((error) => {
   if (error) {
     console.log(error);
   } else {
-    console.log('Ready to Send');
+    console.log("Ready to Send");
   }
 });
 
-app.post('/contact', (req, res) => {
+app.post("/contact", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
@@ -36,7 +36,7 @@ app.post('/contact', (req, res) => {
   const mail = {
     from: name,
     to: process.env.EMAIL_USER, // Use environment variable here too
-    subject: 'Contact Form Submission - Portfolio',
+    subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
            <p>Phone: ${phone}</p>
@@ -46,15 +46,15 @@ app.post('/contact', (req, res) => {
     if (error) {
       res.json(error);
     } else {
-      res.json({ code: 200, status: 'Message Sent' });
+      res.json({code: 200, status: "Message Sent"});
     }
   });
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // Export the Express app as a Cloud Function
