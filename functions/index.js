b0620bd5f2ef39d5afb54f99d1d15e73047814ea
Michael Gnaho <gnahomichael@gmail.com>
2024-07-28 18:49:48 +0100

    Update functions and deploy

---
+++ functions/index.js
@@ -0,0 +1,61 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Ready to Send');
+  }
+});
+
+app.post('/contact', (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: process.env.EMAIL_USER, // Use environment variable here too
+    subject: 'Contact Form Submission - Portfolio',
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: 'Message Sent' });
+    }
+  });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Export the Express app as a Cloud Function
+exports.app = functions.https.onRequest(app);
